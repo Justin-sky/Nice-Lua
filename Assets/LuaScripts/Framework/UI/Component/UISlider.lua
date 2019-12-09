1595ac26a42fa09944a5871604463ef9f2c49b6e
Justin-sky <284139554@qq.com>
2019-12-09 12:08:08 +0800

    slider添加onValueChanged事件绑定

--- Assets/LuaScripts/Framework/UI/Component/UISlider.lua
+++ Assets/LuaScripts/Framework/UI/Component/UISlider.lua
@@ -9,7 +9,7 @@ local UISlider = BaseClass("UISlider", UIBaseComponent)
 local base = UIBaseComponent
 
 -- 创建
-local function OnCreate(self, binder, property_name)
+local function OnCreate(self, binder, property_name,on_value_changed)
 	base.OnCreate(self)
 	-- Unity侧原生组件
 	self.unity_uislider = UIUtil.FindSlider(self.transform)
@@ -20,7 +20,7 @@ local function OnCreate(self, binder, property_name)
 	end
 
 	--添加双向绑定
-	if(binder~=nil and property_name~=nil and not IsNull(self.unity_uiinput)) then
+	if(binder~=nil and property_name~=nil and not IsNull(self.unity_uislider)) then
 		--ViewModel => Input
 		binder:Add(property_name, function (oldValue, newValue)
 			if oldValue ~= newValue then
@@ -28,7 +28,29 @@ local function OnCreate(self, binder, property_name)
 			end
 		end)
 
+		--绑定事件
+		if(on_value_changed ~= nil) then
+			binder:RegisterEvent(function(viewModel, property)
+				self:SetOnValueChanged(function ()
+					local onValueChanged = property['OnValueChanged']
+					if onValueChanged == nil then
+						return
+					end
+					onValueChanged()
+				end)
+			end, function()
+				if self.__onValueChanged ~= nil then
+					self.unity_uislider.onValueChanged:RemoveListener(self.__onValueChanged)
+				end
+			end, on_value_changed)
+		end
 	end
+end
+
+-- 设置回调
+local function SetOnValueChanged(self, ...)
+	self.__onValueChanged = BindCallback(...)
+	self.unity_uislider.onValueChanged:AddListener(self.__onValueChanged)
 end
 
 -- 获取进度
